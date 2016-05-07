ef27e1d16f043bcb76923c8eaa760d2a1a66701d
Adam <Adam@anope.org>
2016-05-07 12:12:43 -0400

    Various fixups, use classifier correctly

--- src/main/bash/update.sh
+++ src/main/bash/update.sh
@@ -3,8 +3,6 @@
 JAVA_ARGS="-ea -Xmx512m"
 FERNFLOWER_JAR=/home/runelite/fernflower/fernflower.jar
 
-DEOB_PATH=`echo $DEOB_PATH | sed 's/\.jar$/-jar-with-dependencies.jar/'`
-
 echo RS API at $RS_API_PATH
 echo RS client $RS_CLIENT_PATH
 echo Deobfuscator at $DEOB_PATH
@@ -14,6 +12,7 @@ VANILLA=/tmp/vanilla.jar
 DEOBFUSCATED=/tmp/deobfuscated.jar
 DEOBFUSCATED_WITH_MAPPINGS=/tmp/deobfuscated_with_mappings.jar
 VANILLA_INJECTED=/tmp/vanilla_injected.jar
+RS_CLIENT_REPO=/home/runelite/runelite
 
 curl -L oldschool.runescape.com/jav_config.ws > $JAV_CONFIG
 
@@ -23,8 +22,8 @@ JAR_URL=$CODEBASE$INITIAL_JAR
 
 echo Downloading vanilla client from $JAR_URL
 
-#rm -f $VANILLA
-#wget $JAR_URL -O $VANILLA
+rm -f $VANILLA
+wget $JAR_URL -O $VANILLA
 
 # get version of vanilla
 VANILLA_VER=$(java -cp $DEOB_PATH net.runelite.deob.clientver.ClientVersionMain $VANILLA)
@@ -49,13 +48,15 @@ jar xf *.jar
 cd -
 
 # update deobfuscated client repository
-cd $RS_CLIENT_REPO
+cd $RS_CLIENT_REPO/runescape-client
 git rm src/main/java/*.java
 mkdir -p src/main/java/
 cp /tmp/dest/*.java src/main/java/
 git add src/main/java/
 
 # bump versions
+find $RS_CLIENT_REPO -name pom.xml -exec sed -i "s/<version>.*<\/version>.*rs version.*/<version>$VANILLA_VER.1-SNAPSHOT<\/version> <!-- rs version -->/" {} \;
+find $RS_CLIENT_REPO -name pom.xml -exec git add {} \;
 
 git config user.name "Runelite auto updater"
 git config user.email runelite@runelite.net
@@ -63,3 +64,12 @@ git config user.email runelite@runelite.net
 git commit -m "Update $VANILLA_VER"
 #git push
 
+# Now update our version
+cd $BASEDIR
+find $BASEDIR -name pom.xml -exec sed -i "s/<version>.*<\/version>.*rs version.*/<version>$VANILLA_VER.1-SNAPSHOT<\/version> <!-- rs version -->/" {} \;
+
+git config user.name "Runelite auto updater"
+git config user.email runelite@runelite.net
+
+git commit -m "Update $VANILLA_VER"
+#git push
